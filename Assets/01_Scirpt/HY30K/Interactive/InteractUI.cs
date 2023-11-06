7da88d0ea8c217ddd64fcb9a8ffb2a733a6ff0ae
Hyeok-06 <jun44227@gmail.com>
2023-11-06 12:01:04 +0900

    Default Setting

---
+++ Assets/01_Scirpt/HY30K/Interactive/InteractUI.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class InteractUI : Interactable
+{
+    protected override void Interact()
+    {
+        
+    }
+}
