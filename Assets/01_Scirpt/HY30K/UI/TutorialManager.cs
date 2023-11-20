a8b88deee329b2b64db3f97a3817d22e68c90248
Hyeok-06 <jun44227@gmail.com>
2023-11-20 12:39:12 +0900

    GIF 제외한 튜토리얼 끝

--- Assets/01_Scirpt/HY30K/UI/TutorialManager.cs
+++ Assets/01_Scirpt/HY30K/UI/TutorialManager.cs
@@ -7,6 +7,7 @@ using DG.Tweening;
 using UnityEngine.UI;
 using TMPro;
 using UnityEngine.Events;
+using Unity.VisualScripting;
 
 [Serializable]
 public class OutputContent
@@ -15,7 +16,7 @@ public class OutputContent
     public float waitTime;
     public float fadeTime; //알파 조정 시간
     public float duration;   //지속 시간
-
+    public Sprite sprite;
     public UnityEvent OnEvent;
 }
 
@@ -24,10 +25,12 @@ public class TutorialManager : MonoBehaviour
     public List<OutputContent> DrawList;
 
     private TextMeshProUGUI _text;
+    private Image _img;
 
     private void Awake()
     {
         _text = transform.Find("TextContent").GetComponent<TextMeshProUGUI>();
+        _img = transform.Find("ImageContent").GetComponent<Image>();
     }
 
     private void Start()
@@ -40,15 +43,17 @@ public class TutorialManager : MonoBehaviour
         for (int i = 0; i < DrawList.Count; i++)
         {
             _text.text = DrawList[i].text;
-
+            _img.sprite = DrawList[i].sprite;
             yield return new WaitForSeconds(DrawList[i].waitTime);
 
             _text.DOFade(1, DrawList[i].fadeTime);
+            _img.DOFade(1, DrawList[i].fadeTime);
             if (DrawList[i].OnEvent != null)
             {
                 DrawList[i].OnEvent?.Invoke();
             }
             yield return new WaitForSeconds(DrawList[i].duration);
+            _text.DOFade(0, DrawList[i].fadeTime);
             _text.DOFade(0, DrawList[i].fadeTime);
             yield return new WaitForSeconds(DrawList[i].fadeTime);
         }
